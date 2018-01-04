c33bbc4efc03291b92daa1402e7aba2257cc7acb
drastega <leda82@gmail.com>
2018-01-04 15:57:47 +0300

    Speckles with normal energy distribution were added

--- speckle_gen.R
+++ speckle_gen.R
@@ -4,21 +4,33 @@ library(rgl)
 
 # speckle_gen <- function(rho = 0.5, theta = 0, dm = 0, seeing = 1, wind_speed = 0, N_frames = 1){}
 
-N_pix <- 512 * 512 # Number of pixels in the frame
+n_x <- 512; n_y <- n_x # Number of pixels in x and y
 sigma_noise <- 30
 mean_noise <- 400
-noise <- runif(N_pix) * sigma_noise + mean_noise %>% matrix(512, 512)
-N_speckle <- 300
+noise <- runif(n_x * n_y) * sigma_noise + mean_noise %>% matrix(512, 512)
+
 seeing <- 40 # in relative units
-wind_speed <- 20 # in relative units
-x_center <- rnorm(1, mean = 257, sd = wind_speed) ; y_center <- rnorm(1, mean = 257, sd = wind_speed)
+wind_speed <- 0 # in relative units
 
-#speckle_frame <- matrix(0, 512, 512)
+N_speckle <- 300 # Number of speckles
+speckle_amplitude <- 20; speckle_sigma <- 3
 
-### Visualization
-plot(as.cimg(noise))
-points(x = rnorm(N_speckle, mean = x_center, sd = seeing), y = rnorm(N_speckle, mean = y_center, sd = seeing),
-       col = 'white', pch = 20)
+gaussian_2d <- matrix(0, nrow = n_x, ncol = n_y)
+speckle_field <- matrix(0, nrow = n_x, ncol = n_y)
 
-#open3d(useNULL = FALSE, windowRect=c(1,1,512,512))
-#persp3d(z = noise, col = 'green')
+stellar_center_x <- rnorm(1, mean = 257, sd = wind_speed)
+stellar_center_y <- rnorm(1, mean = 257, sd = wind_speed)
+
+for (i in seq(N_speckle)) {
+  x0 <- rnorm(1, mean = stellar_center_x, sd = seeing)
+  y0 <- rnorm(1, mean = stellar_center_y, sd = seeing)
+  for (x in seq(n_x)) {
+    for (y in seq(n_y)) {
+    gaussian_2d[x, y] <- speckle_amplitude * exp(-( (x - x0)^2/(2 * speckle_sigma^2) + (y - y0)^2/(2 * speckle_sigma^2)))
+    }
+  }
+  speckle_field <- speckle_field + gaussian_2d
+}
+
+speckle_frame <- speckle_field + noise
+plot(as.cimg(speckle_frame))
