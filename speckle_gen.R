c7def16be38b6a402775bb55de05abc2d08048e5
drastega <leda82@gmail.com>
2018-01-06 02:08:20 +0300

    first working version with Rcpp and inline

--- speckle_gen.R
+++ speckle_gen.R
@@ -39,53 +39,44 @@ int N_speckle = 300;
 int n_x = 512;
 int n_y = 512;
 
-double seeing = 40;
-double rho_x = 10;
-double rho_y = 10;
+double seeing = 30;
+double rho_x = 30;
+double rho_y = 30;
 double speckle_amplitude1 = 200;
 double speckle_amplitude2 = 200;
-double speckle_sigma = 2;
-
-//NumericMatrix speckle_field(512, 512);
-//NumericMatrix gaussian_2d(512, 512);
+double speckle_sigma = 1.5;
 
 std::vector<double> speckle_field(262144);
 std::vector<double> gaussian_2d(262144);
 
-double stellar_center_x;
-double stellar_center_y;
+double stellar_center_x = 257;
+double stellar_center_y = 257;
 double x0;
 double y0;
 
 for (int i = 0; i < N_speckle; i++) {
-  x0 = rand()%40 - (seeing / 2) + 257 - (rho_x / 2);
-  y0 = rand()%40 - (seeing / 2) + 257 - (rho_y / 2);
-  for (int x = 0;  x < n_x + 1; x++) {
-    for (int y = 0; y < n_y + 1; y++) {
-//    gaussian_2d[x + y] = speckle_amplitude1 * exp(-(pow((x - x0), 2)/(2 * pow(speckle_sigma, 2)) + pow((y - y0), 2)/(2 * pow(speckle_sigma, 2))));
-//+ speckle_amplitude2 * exp(-(pow((x - x0 - rho_x), 2)/(2 * pow(speckle_sigma, 2)) + pow((y - y0 - rho_y), 2)/(2 * pow(speckle_sigma, 2))));
-  gaussian_2d[x * n_y + y] = x * y;
+  x0 = R::rnorm(stellar_center_x, seeing) - rho_x / 2;
+  y0 = R::rnorm(stellar_center_y, seeing) - rho_y / 2;
+  for (int x = 0;  x < n_x; x++) {
+    for (int y = 0; y < n_y; y++) {
+    gaussian_2d[x * n_y + y] = speckle_amplitude1 * exp(-(pow((x - x0), 2)/(2 * pow(speckle_sigma, 2)) + pow((y - y0), 2)/(2 * pow(speckle_sigma, 2)))) + speckle_amplitude2 * exp(-(pow((x - x0 - rho_x), 2)/(2 * pow(speckle_sigma, 2)) + pow((y - y0 - rho_y), 2)/(2 * pow(speckle_sigma, 2))));
+//  gaussian_2d[x * n_y + y] = x * y;
     }
   }
   for (int c = 0 ; c < 262144 ; c++) {
     speckle_field[c] = speckle_field[c] + gaussian_2d[c];
   }
 }
-return Rcpp::wrap(gaussian_2d);
+return Rcpp::wrap(speckle_field);
+//return Rcpp::wrap(dist_r);
 '
 
-src1 <- '
-int x = rand()%20 + 247;
-return Rcpp::wrap(x);
-'
-
-#speckle_cpp <- rcpp(signature(), src1, body = 'return Rcpp::wrap( speckle_field );')
-speckle_cpp <- rcpp(signature(), src)#, body = 'int x = Rcpp::as<int>(n_x);')
+speckle_cpp <- rcpp(signature(), src)
 speckle_field <- speckle_cpp() %>% matrix(512, 512)
 
 speckle_frame <- speckle_field + noise
 plot(as.cimg(speckle_frame), axes = FALSE)
 print(Sys.time() - t0)
 
-PS_model <- abs(fftw2d(speckle_frame))^2 %>% fftshift(dimension = -1)
-plot(as.cimg(PS_model^0.001), axes = FALSE)
+#PS_model <- abs(fftw2d(speckle_frame))^2 %>% fftshift(dimension = -1)
+#plot(as.cimg(PS_model^0.001), axes = FALSE)
