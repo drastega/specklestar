28fbc3d6b6452231faea6c105188a4a6ed5d40fc
drastega <leda82@gmail.com>
2018-01-25 23:00:21 +0300

    add real difference of neighboring frames to ps_diff.cpp

--- src/ps_diff.cpp
+++ src/ps_diff.cpp
@@ -23,38 +23,47 @@ using namespace Rcpp;
 //' @export
 // [[Rcpp::export]]
 NumericVector ps_diff(String filename, std::size_t threshold = 50000) {
+  size_t frameSize = IMAGE_SIZE * sizeof(unsigned short);
+
   std::ifstream file(filename, std::ios::binary);
   file.seekg(0, std::ios::end);
   size_t file_length = file.tellg();
   file.seekg(0, std::ios::beg);
 
-  int N_frame = file_length / (IMAGE_SIZE * sizeof(unsigned short));
-  unsigned short piData1[IMAGE_SIZE];
-  unsigned short piData2[IMAGE_SIZE];
-//  std::vector<double> big_dData(1024*1024);
-  NumericMatrix big_dData(1024, 1024);
   NumericMatrix outData(513, 1024);
+  NumericMatrix big_dData(1024, 1024);
+
+  int N_frame = file_length / frameSize;
+  unsigned short piData1[IMAGE_SIZE]
+               , piData2[IMAGE_SIZE]
+               ;
 
   fftw_complex *out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * 1024 * (1024 / 2 + 1));
-  for(int j = 0; j < N_frame; j++){
-    file.read((char*)piData1, IMAGE_SIZE * sizeof(unsigned short));
+
+  // int state = 0;
+  bool state = true;
+  int j = 0;
+  while( file.read((char*)piData1, frameSize) ) {
+    j++;
     if (IsOverThresholdFrame(piData1, threshold)) continue;
-    file.read((char*)piData2, IMAGE_SIZE * sizeof(unsigned short));
-    if (IsOverThresholdFrame(piData2, threshold)) continue;
+    break;
+  }
+  for(; j < N_frame; j++) {
+    file.read((char*)(state ? piData1 : piData2), frameSize);
+    if (IsOverThresholdFrame((state ? piData1 : piData2), threshold)) continue;
 
     for(int i = 0; i < 512; i++) {
       for(int j = 0; j < 512; j++) {
-        big_dData[j * 1024 + i] = (double)piData2[i * 512 + j] - (double)piData1[i * 512 + j];
+        big_dData[j * 1024 + i] = (double)(state ? piData1 : piData2)[i * 512 + j]
+                                - (double)(state ? piData2 : piData1)[i * 512 + j];
       }
     }
-
-    file.seekg(-IMAGE_SIZE * sizeof(unsigned short), std::ios_base::cur); // shift pointer one frame back
-
     fftw_plan p = fftw_plan_dft_r2c_2d(1024, 1024, big_dData.begin(), out, FFTW_ESTIMATE);
-//    fftw_plan p = fftw_plan_dft_r2c_2d(1024, 1024, big_dData.data(), out, FFTW_ESTIMATE);
-    fftw_execute(p); /* repeat as needed */
+    fftw_execute(p); // repeat as needed
     fftw_destroy_plan(p);
     for (int i = 0; i < 1024 * 513; i++) outData[i] += out[i][0] * out[i][0] + out[i][1] * out[i][1];
+    // state = 0 == state ? 1 : 0;
+    state = !state;
   }
   fftw_free(out);
   file.close();
