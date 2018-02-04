5b09b337cf7a946c457026762554f63b09896117
drastega <leda82@gmail.com>
2018-02-04 21:16:00 +0300

    remove par and plot from speckle_frame.R

--- R/speckle_frame.R
+++ R/speckle_frame.R
@@ -4,7 +4,7 @@
 #'
 #' @param data_file A string.
 #' @param frame An integer.
-#' @return NULL.
+#' @return 512 x 512 matrix with given frame
 #' @examples
 #' ## Plot frame number 57 from file you choose
 #' # speckle_frame(, 57) # no first argument here
@@ -21,12 +21,9 @@ speckle_frame <- function(data_file = file.choose(), frame = 1) {
   file_connector = file(tmp_file, "rb")
   frame <- readBin(file_connector, integer(), endian = "little", n = 512 * 512, size = 2)
   frame <- matrix(frame, 512, 512)
-  par(mar = c(0, 0, 0, 0))
-  plot(imager::as.cimg(frame^0.1), axes = FALSE)
-  par(mar = c(5.1, 4.1, 4.1, 2.1)) # set standard margin parameters
 
   close(file_connector)
   file.remove(tmp_file)
 
-  return(invisible())
+  return(frame)
 }
