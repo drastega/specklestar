7227d45d912fad1d7d2e4fb11a06185b70a1d6c4
drastega <leda82@gmail.com>
2018-02-04 20:57:07 +0300

    rm middle_frame.R, speckle_binary.R; fix specklestar.R; clean misc_functions.R

--- R/specklestar.R
+++ R/specklestar.R
@@ -1,25 +1,12 @@
 #' specklestar: A package for reduction of speckle data.
 #'
-#' The specklestar package provides three categories of important functions:
-#' foo, bar and baz.
-#'
-#' @section Foo functions:
-#' The foo functions ...
+#' The specklestar package provides functions for obtaining
+#' power spectrum and autocorrelation function from speckle data.
 #'
 #' @docType package
 #' @name specklestar
 #'
+#' @useDynLib specklestar
 #' @import fftw
+#' @importFrom Rcpp sourceCpp
 NULL
-
-options(device = "quartz") # For OSX only
-# options(device = "RStudioGD") # RStudio default graphics device
-
-## Functions
-
-#- n_frames(N) - take N frames from series of 512x512x2(bytes) speckle images
-#- spe2dat(filename) - SPE to dat conversion (i.e. removing 4100 bytes SPE header)
-#- middle_frame(filename) - calculate average image of the series of speckle images
-#- ps(filename) - calculate power spectrum
-#- speckle_generator()- generate speckle pattern of binary star
-#- speckle_binary(filename) - Calculation rho, theta and dm for binary star
