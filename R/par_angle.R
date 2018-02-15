6759205ed200ccc75ad63ca59818c6ffb8beb6fd
drastega <leda82@gmail.com>
2018-02-16 00:20:45 +0300

    add read.table to par_angle.R

--- R/par_angle.R
+++ R/par_angle.R
@@ -1,57 +1,55 @@
-library(tidyverse)
-library(celestial)
-#' Calculate parallactic angle
+#' Calculate parallactic angles from BTA log file
 #'
 #' @param log_file a character string with the path name to a log file.
+#' @param log_date a character string with date of observations (YYYY-MM-DD).
 #' @return Tibble with names and parallactic angles calculated from BTA log file.
 #' @examples
 #' log_filename <- system.file("extdata", "2010-02-27.tel", package = "specklestar")
-#' par_angle(log_filename)
+#' par_angle(log_filename, '2010-02-27')
 #' @export
-par_angle <- function(log_file = file.choose()) {
-column_names <- c('Name', 'Alpha_h', 'Alpha_m', 'Alpha_s', 'Delta_d', 'Delta_m', 'Delta_s', 'Mtime', 'Stime', 'z', 'Focus')
+par_angle <- function(log_file = file.choose(), log_date) {
+  library(tidyverse)
+  library(celestial)
 
-log_data <- read_table(log_file, col_names = column_names, col_types = cols(
-  'Name' = col_character(), 'Alpha_h' = col_character(), 'Alpha_m' = col_integer(), 'Alpha_s' = col_double(),
-  'Delta_d' = col_character(), 'Delta_m' = col_integer(), 'Delta_s' = col_double(), 'Mtime' = col_skip(),
-  'Stime' = col_character(), 'z' = col_skip(), 'Focus' = col_skip()))
+  column_names <- c('Name', 'Alpha_h', 'Alpha_m', 'Alpha_s', 'Delta_d', 'Delta_m', 'Delta_s', 'Mtime', 'Stime', 'z', 'Focus')
 
-log_data <- log_data %>% na.omit()
+  log_data <- read.table(log_file, col.names = column_names)
 
-BTA_latitude_rad <- 0.761894396137 # +43 deg 39' 12''
-log_date <- tools::file_path_sans_ext(basename(log_file))
+  log_data <- log_data %>% na.omit()
 
-log_data <- log_data %>%
-  mutate(Alpha_h = as.integer(str_replace(Alpha_h, 'Alpha=', ''))) %>%
-  mutate(Alpha = paste(Alpha_h, Alpha_m, Alpha_s, sep = ':')) %>%
-  mutate(Alpha_rad = hms2deg(Alpha) * pi / 180) %>%
+  BTA_latitude_rad <- 0.761894396137 # +43 deg 39' 12''
 
-  mutate(Delta_d = str_replace(Delta_d, 'Delta=', '')) %>%
-  mutate(Delta_rad = paste(Delta_d, Delta_m, Delta_s, sep = ':')) %>%
-  mutate(Delta_rad = dms2deg(Delta_rad) * pi / 180) %>%
+  log_data <- log_data %>%
+    mutate(Alpha_h = as.integer(str_replace(Alpha_h, 'Alpha=', ''))) %>%
+    mutate(Alpha = paste(Alpha_h, Alpha_m, Alpha_s, sep = ':')) %>%
+    mutate(Alpha_rad = hms2deg(Alpha) * pi / 180) %>%
 
-  mutate(Stime = str_replace(Stime, 'Stime=', '')) %>%
-  mutate(Stime_s = as.integer(as.integer(str_sub(Stime, start = -1)) * 6)) %>%
-  mutate(Stime = str_sub(Stime, start = 1, end = 5)) %>%
-  mutate(Stime = paste(Stime, Stime_s, sep = ':')) %>%
-  mutate(Stime_rad = hms2deg(Stime) * pi / 180) %>%
+    mutate(Delta_d = str_replace(Delta_d, 'Delta=', '')) %>%
+    mutate(Delta_rad = paste(Delta_d, Delta_m, Delta_s, sep = ':')) %>%
+    mutate(Delta_rad = dms2deg(Delta_rad) * pi / 180) %>%
 
-  mutate(Sdate = paste(log_date, Stime, sep = ' ')) %>%
-  mutate(Sdate = lubridate::ymd_hms(Sdate)) %>%
+    mutate(Stime = str_replace(Stime, 'Stime=', '')) %>%
+    mutate(Stime_s = as.integer(as.integer(str_sub(Stime, start = -1)) * 6)) %>%
+    mutate(Stime = str_sub(Stime, start = 1, end = 5)) %>%
+    mutate(Stime = paste(Stime, Stime_s, sep = ':')) %>%
+    mutate(Stime_rad = hms2deg(Stime) * pi / 180) %>%
 
-  group_by(Name) %>% # vulnerable place!
-  mutate(mean_Sdate = mean(Sdate)) %>%
-  mutate(mean_Alpha_rad = mean(Alpha_rad)) %>%
-  mutate(mean_Delta_rad = mean(Delta_rad)) %>%
-  mutate(mean_Stime_rad = mean(Stime_rad)) %>%
+    mutate(Sdate = paste(log_date, Stime, sep = ' ')) %>%
+    mutate(Sdate = lubridate::ymd_hms(Sdate)) %>%
 
-  mutate(Hour_angle_rad = mean_Stime_rad - mean_Alpha_rad) %>%
+    group_by(Name) %>% # vulnerable place!
+    mutate(mean_Sdate = mean(Sdate)) %>%
+    mutate(mean_Alpha_rad = mean(Alpha_rad)) %>%
+    mutate(mean_Delta_rad = mean(Delta_rad)) %>%
+    mutate(mean_Stime_rad = mean(Stime_rad)) %>%
 
-  mutate(Q_degr = atan(sin(Hour_angle_rad) / (tan(BTA_latitude_rad) * cos(mean_Delta_rad) -
+    mutate(Hour_angle_rad = mean_Stime_rad - mean_Alpha_rad) %>%
+
+    mutate(Q_degr = atan(sin(Hour_angle_rad) / (tan(BTA_latitude_rad) * cos(mean_Delta_rad) -
                                      sin(mean_Delta_rad) * cos(Hour_angle_rad))) * 180 / pi) %>%
 
-  select(c(Name, Q_degr)) %>%
-  distinct()
+    select(c(Name, Q_degr)) %>%
+    distinct()
 
-return(log_data)
+  return(log_data)
 }
