207c7ced464faa2a2268acc898dc4c55812adc7d
drastega <leda82@gmail.com>
2018-02-20 17:11:19 +0300

    add Date to database in obs_database.R

--- R/obs_database.R
+++ R/obs_database.R
@@ -3,7 +3,7 @@
 #' Returns data frame of BTA observational journals
 #'
 #' @param logs_dir a character string of path to folder with observational journals
-#' @return Data frame of observational journals.
+#' @return Tibble with observational journals.
 #' @examples
 #' db <- obs_database()
 #' @export
@@ -11,7 +11,7 @@ obs_database <- function(logs_dir = NULL) {
   library(tidyverse)
 
   if (is.null(logs_dir)) {
-    logs_dir <- '/Volumes/E/Data/new_telescope'
+    logs_dir <- '/Volumes/E/Data/new_telescope_EDIT/'
     print(logs_dir)
   }
 
@@ -33,9 +33,11 @@ obs_database <- function(logs_dir = NULL) {
   column_names <- c('Name', 'Alpha_h', 'Alpha_m', 'Alpha_s', 'Delta_d', 'Delta_m', 'Delta_s', 'Mtime', 'Stime', 'z', 'Focus')
 
   database <- read.table(included_files[1], col.names = column_names)
+  database$Date <- basename(tools::file_path_sans_ext(included_files[1]))
 
   for (file in included_files[-1]){
     night_database <- read.table(file, col.names = column_names)
+    night_database$Date <- basename(tools::file_path_sans_ext(file))
     database <- rbind(database, night_database)
   }
 
@@ -47,8 +49,8 @@ obs_database <- function(logs_dir = NULL) {
     mutate(Alpha_rad = celestial::hms2deg(Alpha) * pi / 180) %>%
 
     mutate(Delta_d = str_replace(Delta_d, 'Delta=', '')) %>%
-    mutate(Delta_rad = paste(Delta_d, Delta_m, Delta_s, sep = ':')) %>%
-    mutate(Delta_rad = celestial::dms2deg(Delta_rad) * pi / 180) %>%
+    mutate(Delta = paste(Delta_d, Delta_m, Delta_s, sep = ':')) %>%
+    mutate(Delta_rad = celestial::dms2deg(Delta) * pi / 180) %>%
 
     mutate(Stime = str_replace(Stime, 'Stime=', '')) %>%
     mutate(Stime_s = as.integer(as.integer(str_sub(Stime, start = -1)) * 6)) %>%
@@ -57,15 +59,22 @@ obs_database <- function(logs_dir = NULL) {
     mutate(Stime = str_replace(Stime, '60:0', '59:59')) %>%
     mutate(Stime_rad = celestial::hms2deg(Stime) * pi / 180) %>%
 
-#    mutate(Sdate = paste(log_date, Stime, sep = ' ')) %>%
+#    mutate(Sdate = paste(Date, Stime, sep = ' ')) %>%
 #    mutate(Sdate = lubridate::ymd_hms(Sdate)) %>%
 
     mutate(Mtime = str_replace(Mtime, 'Mtime=', '')) %>%
     mutate(Mtime_s = as.integer(as.integer(str_sub(Mtime, start = -1)) * 6)) %>%
     mutate(Mtime = str_sub(Mtime, start = 1, end = 5)) %>%
-    mutate(Mtime = paste(Mtime, Mtime_s, sep = ':'))
-#    mutate(Mdate = paste(log_date, Mtime, sep = ' ')) %>%
-#    mutate(Mdate = lubridate::ymd_hms(Mdate))
+    mutate(Mtime = paste(Mtime, Mtime_s, sep = ':')) %>%
+#    mutate(Mdate = paste(Date, Mtime, sep = ' ')) %>%
+#    mutate(Mdate = lubridate::ymd_hms(Mdate)) %>%
+
+    mutate(z = str_replace(z, 'Z=', '')) %>%
+    mutate(Focus = str_replace(Focus, 'Focus=', '')) %>%
+    mutate(Date = lubridate::dmy(Date)) %>%
+
+    select(c(Name, Alpha, Delta, Mtime, Stime, Date, z, Focus)) %>%
+    as.tibble()
 
   return(database)
 }
