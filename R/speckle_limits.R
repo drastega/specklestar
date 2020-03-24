15eb1d7820578c88ec52ed57f966466b9668a6c6
denis <denis@aytm.com>
2020-03-24 16:51:58 +0300

    fix speckle_limits for 2'' plot

--- R/speckle_limits.R
+++ R/speckle_limits.R
@@ -4,7 +4,7 @@
 #' using formalism described in 2010AJ....139..743T (Tokovinin, Mason & Hartkopf, 2010)
 #'
 #' @param im matrix with acf.
-#' @param band a character string '550', '600' or '800'.
+#' @param band a character string '550', '600', '700', or '800'.
 #' @param rho_lim limit in arc seconds (default is 1'', maximum is 2'').
 #' @return List of x and y with limits (x is rho and y is dm).
 #' @examples
@@ -16,15 +16,17 @@
 speckle_limits <- function(im, band, rho_lim = 1) {
 
   fluct <- vector()
-  for (i in seq(1, 200, 2)) {
+  for (i in seq(1, 250, 2)) {
     annulus_points <- annulus(im, i)
     fluct <- c(fluct, sqrt(mean(annulus_points^2)))
   }
+
   scale_550 <- 0.00885267819
   scale_600 <- scale_550 * 0.99651
+  scale_700 <- scale_550 * 0.9922462
   scale_800 <- scale_550 * 0.98751
 
-  N <- ceiling(rho_lim * 50)
+  N <- ceiling(rho_lim * 58)
 
   if (band == '550') {
     x <- seq(1, N) * scale_550 * 2
@@ -32,6 +34,10 @@ speckle_limits <- function(im, band, rho_lim = 1) {
 
   if (band == '600') {
     x <- seq(1, N) * scale_600 * 2
+  }
+
+  if (band == '700') {
+    x <- seq(1, N) * scale_700 * 2
   }
 
   if (band == '800') {
